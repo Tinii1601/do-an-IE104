91814a85fd03cc16955a3a5269ef1b27303a2829
Tinii1601 <22521471@gm.uit.edu.vn>
2024-11-09 18:37:37 +0700

    cai tailwind css

--- src/views/App.js
+++ src/views/App.js
@@ -1,12 +1,12 @@
 import React from "react";
+import Header from "../components/Header";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello, World!</h1>
-        <p>This is a React app.</p>
-      </div>
+      <>
+        <Header />
+      </>
     );
   }
 }

e7856f28d8c11ef381a1c6437b30138d5fd8ff78
Tinii1601 <22521471@gm.uit.edu.vn>
2024-11-10 20:28:29 +0700

    header lan 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,72 @@ import React from "react";
 
 class Header extends React.Component {
   render() {
-    return <></>;
+    return (
+      <>
+        <header>
+          <div className="bg-green flex justify-center items-center space-x-4">
+            <p className="text-white p-4 text-2xl">
+              Hotline: 1900xxxx Email: hotro@nhasach.com
+            </p>
+            <img
+              src={require("../assets/icons/facebook.png")}
+              alt="Facebook Icon"
+              width={40}
+              height={40}
+            ></img>
+            <img
+              src={require("../assets/icons/messenger.png")}
+              alt="Messenger Icon"
+              width={40}
+              height={40}
+            ></img>
+            <img
+              src={require("../assets/icons/zalo.jpeg")}
+              alt="Zalo Icon"
+              width={40}
+              height={40}
+            ></img>
+          </div>
+          <div className="flex items-center justify-between mt-5 mx-5">
+            <div className="w-52 h-14 rounded-2xl bg-red mx-5 text-white text-center">
+              Logo
+            </div>
+            <div className="relative w-2/4">
+              <img
+                src={require("../assets/icons/icon_search.png")}
+                alt="Search Icon"
+                className="absolute left-4 top-1/2 transform -translate-y-1/2"
+                width={30}
+                height={30}
+              />
+              <input
+                type="text"
+                placeholder="Tìm kiếm sản phẩm"
+                className="w-full h-14 rounded-2xl border-black border-solid pl-12 pr-5"
+              />
+            </div>
+            <button className="w-40 h-14 bg-orange rounded-2xl text-white text-2xl font-bold flex items-center justify-center space-x-2">
+              <img
+                src={require("../assets/icons/icon_user.png")}
+                alt="User Icon"
+                width={30}
+                height={30}
+              />
+              <span>Tài khoản</span>
+            </button>
+            <button className="w-40 h-14 bg-orange rounded-2xl text-white text-2xl font-bold flex items-center justify-center space-x-2">
+              <img
+                src={require("../assets/icons/icon_cart.png")}
+                alt="Cart Icon"
+                width={30}
+                height={30}
+              />
+              <span>Giỏ hàng</span>
+            </button>
+          </div>
+        </header>
+      </>
+    );
   }
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "#72B01D",
+        orange: "#FF7F11",
+        red: "#FF0E0E",
+        white_pink: "#FFF8F0",
+      },
+      fontFamily: {
+        Roboto1: ['"Roboto"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };

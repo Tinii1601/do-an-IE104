a8d8a9889a29a6ea90b509b6741a335ca4503fd1
tishiu <tranmap04@gmail.com>
2024-11-16 14:40:48 +0700

    MP

--- src/views/App.js
+++ src/views/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Product from "../components/Product";
 import "../styles/App.css";
+import Account from "../components/Account";
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header />
-        <Product />
+        <Account />
         <Footer />
       </>
     );

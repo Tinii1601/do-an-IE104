5b7f3418bb0533963504691e292e5ff1780a5684
Tinii1601 <22521471@gm.uit.edu.vn>
2024-11-26 14:18:41 +0700

    update

--- src/views/Product_Detail.js
+++ src/views/Product_Detail.js
@@ -9,11 +9,17 @@ class ProductDetail extends React.Component {
         <div className="relative z-20">
           <Header />
         </div>
-        <div>
-          <p>Hai Vạn Dặm Dưới Biển</p>
-          <img
-            src={require("../assets/images/hai-van-dam-duoi-bien-tb-2024.jpg")}
-          />
+        <div className="my-5 mx-10 h-auto bg-white p-5">
+          <p className="text-2xl font-bold ml-5">Hai Vạn Dặm Dưới Biển</p>
+          <div className="flex ">
+            <div className="relative basis-3/5">
+              <img
+                className="absolute w-1/2 mt-5 left-1/4"
+                src={require("../assets/images/hai-van-dam-duoi-bien-tb-2024.jpg")}
+              />
+            </div>
+            <div className="basis-2/5 bg-pink-200">Hello</div>
+          </div>
         </div>
         <Footer />
       </>
